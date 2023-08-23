96df1f8531d97e7aae95141386fbbc24f119e15d
gabrielbaseggio <baseggioaxel@gmail.com>
2023-08-23 18:30:43 -0400

    refactoring

--- practico1/src/main/java/mineField/Minefield.java
+++ practico1/src/main/java/mineField/Minefield.java
@@ -57,43 +57,81 @@ public class Minefield {
     	}
     	
     	int n = 0;
-    	if(x-1 >= 0 && y-1 >= 0 && field[x-1][y-1].isMined()) 
-    	{
-    		n++;
-    	}
-    	
-    	if(x-1 >= 0 && y+1 <= 7 && field[x-1][y+1].isMined()) 
-    	{
-    		n++;
-    	}
-    	
-    	if(y-1 >= 0 && field[x][y-1].isMined()) 
-    	{
-    		n++;
-    	}
-    	
-    	if(y+1 <= 7 && field[x][y+1].isMined()) 
-    	{
-    		n++;
-    	}
-    	
-    	if(x+1 <= 7 && y-1 >= 0 && field[x+1][y-1].isMined()) 
-    	{
-    		n++;
-    	}
-    	
-    	if(x+1 <= 7 && field[x+1][y].isMined()) 
-    	{
-    		n++;
-    	}
-    	
-    	if(x+1 <= 7 && y+1 <= 7 && field[x+1][y+1].isMined()) 
-    	{
-    		n++;
-    	}
+    	n = topLeftIsMined(x, y)
+    			+ topIsMined(x, y) 
+    			+ topRightIsMined(x, y) 
+    			+ rightIsMined(x, y) 
+    			+ leftIsMined(x, y)
+    			+ bottomLeftIsMined(x, y)
+    			+ bottomIsMined(x, y)
+    			+ bottomRightIsMined(x, y);
     	
     	
 		return n;
+	}
+
+	private int bottomRightIsMined(int x, int y) {
+		if(x+1 <= 7 && y+1 <= 7 && field[x+1][y+1].isMined()) 
+    	{
+    		return 1;
+    	}
+		return 0;
+	}
+
+	private int bottomIsMined(int x, int y) {
+		if(x+1 <= 7 && field[x+1][y].isMined()) 
+    	{
+    		return 1;
+    	}
+		return 0;
+	}
+
+	private int bottomLeftIsMined(int x, int y) {
+		if(x+1 <= 7 && y-1 >= 0 && field[x+1][y-1].isMined()) 
+    	{
+    		return 1;
+    	}
+		return 0;
+	}
+
+	private int rightIsMined(int x, int y) {
+		if(y+1 <= 7 && field[x][y+1].isMined()) 
+    	{
+    		return 1;
+    	}
+		return 0;
+	}
+
+	private int leftIsMined(int x, int y) {
+		if(y-1 >= 0 && field[x][y-1].isMined()) 
+    	{
+    		return 1;
+    	}
+		return 0;
+	}
+
+	private int topRightIsMined(int x, int y) {
+		if(x-1 >= 0 && y+1 <= 7 && field[x-1][y+1].isMined()) 
+    	{
+    		return 1;
+    	}
+		return 0;
+	}
+	
+	private int topIsMined(int x, int y) {
+		if(x-1 >= 0 && field[x-1][y].isMined()) 
+    	{
+    		return 1;
+    	}
+		return 0;
+	}
+
+	private int topLeftIsMined(int x, int y) {
+		if(x-1 >= 0 && y-1 >= 0 && field[x-1][y-1].isMined()) 
+    	{
+    		return 1;
+    	}
+		return 0;
 	}
 
 	
