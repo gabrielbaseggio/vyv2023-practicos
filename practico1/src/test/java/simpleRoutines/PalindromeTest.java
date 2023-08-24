bda6ddede1aa39a9d1ff09c9efa6778a95706473
gabrielbaseggio <baseggioaxel@gmail.com>
2023-08-24 14:05:52 -0300

    testPalindrome2 test was added

--- practico1/src/test/java/simpleRoutines/PalindromeTest.java
+++ practico1/src/test/java/simpleRoutines/PalindromeTest.java
@@ -2,6 +2,7 @@ package simpleRoutines;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.stream.Stream;
@@ -23,10 +24,27 @@ public class PalindromeTest {
 				);
 	}
 	
+	private static Stream<char[]> notPalindromes() 
+	{
+		return Stream.of(
+				   new char [] {'a', 'b'}
+				  ,new char [] {'a', 'b', 'b'}
+				  ,new char [] {'a', 'b', 'b', 'b'}
+				  ,new char [] {'n', 'o', 't'}
+				);
+	}
+	
 	@ParameterizedTest
 	@MethodSource("palindromes")
 	void testPalindrome1(char [] array) 
 	{
 		assertTrue(Palindrome.palindrome(array));
+	}
+	
+	@ParameterizedTest
+	@MethodSource("notPalindromes")
+	void testPalindrome2(char [] array) 
+	{
+		assertFalse(Palindrome.palindrome(array));
 	}
 }
