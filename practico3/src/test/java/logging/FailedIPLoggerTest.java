d338b0ff083ad4bb10464d4aa28592a5dbd83c42
gabrielbaseggio <baseggioaxel@gmail.com>
2023-09-04 18:17:22 -0400

    the suite of tests was refactored

--- practico3/src/test/java/logging/FailedIPLoggerTest.java
+++ practico3/src/test/java/logging/FailedIPLoggerTest.java
@@ -1,14 +1,25 @@
 package logging;
 
 import static org.easymock.EasyMock.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class FailedIPLoggerTest {
 	private FailedIPLogger logger;
     private LoginService service;
+    private String ip       = "192.186.0.236";
+	private String user     = "sirgabriel";
+	private String password = "12345";
+	private String passwordHash = Utils.getPasswordHashMD5(password);
 
     @BeforeEach
     public  void setUp() {
@@ -17,19 +28,26 @@ public class FailedIPLoggerTest {
          logger.setService(service);
     }
     
-    @Test
-    void
-    loginFailed() 
+    private static Stream<Arguments>
+    provider() 
     {
-    	String ip       = "192.186.0.236";
-    	String user     = "sirgabriel";
-    	String password = "12345";
+    	return Stream.of(
+    			 Arguments.of("192.186.0.236", "sirgabriel", "12345", true, false)
+    			,Arguments.of("192.186.0.236", "sirgabriel", "12345", false, true)
+    			);
+    }
+    
+    @ParameterizedTest
+    @MethodSource("provider")
+    void
+    testLogin(String ip, String user, String password, boolean expForMock, boolean expForAssert) 
+    {
     	String passwordHash = Utils.getPasswordHashMD5(password);
-    	expect(service.login(ip, user, passwordHash)).andReturn(false);
+    	expect(service.login(ip, user, passwordHash)).andReturn(expForMock);
     	replay(service);
     	
     	logger.login(ip, user, password);
-    	assertTrue(logger.loginFailed(ip));
+    	assertEquals(expForAssert, logger.loginFailed(ip));
     	
     	verify(service);
     }
