ffc6a3afc554d68b594d06563a6cb73c8d0a43ec
gabrielbaseggio <baseggioaxel@gmail.com>
2023-10-02 12:17:32 -0400

    pom file was modified

--- practico4/gen-randoop.sh
+++ practico4/gen-randoop.sh
@@ -8,4 +8,4 @@ packagename=${fullclassname%\.*}
 timelimit=$2
 
 
-java -classpath target/classes:libs/randoop-all-3.0.8.jar randoop.main.Main gentests --testclass="$fullclassname" --timelimit="$timelimit" --junit-output-dir=src/test/java --junit-package-name="$packagename" --forbid-null=false --null-ratio=0.5 --npe-on-null-input=ERROR --npe-on-non-null-input=ERROR #--unchecked-exception=ERROR
+java -classpath target/classes:libs/randoop-all-3.0.8.jar randoop.main.Main gentests --testclass="$fullclassname" --timelimit="$timelimit" --junit-output-dir=src/test/java --junit-package-name="$packagename" --forbid-null=false --null-ratio=0.5 --npe-on-null-input=ERROR --npe-on-non-null-input=ERROR --unchecked-exception=ERROR
